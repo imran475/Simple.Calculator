b7d8e75f2e1bb3d74ffcc58efa2f8e0b8d313b17
Imran <146696268+imran475@users.noreply.github.com>
2024-03-20 08:47:34 -0700

    Create Calculator
    Typescript simple Calculator

---
+++ index.js
@@ -0,0 +1,28 @@
+import inguirer from "inquirer";
+// Asking Question from Users through inquirer
+let answers = await inguirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operater to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    },
+]);
+// condtional statements if-else 
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtractions") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
